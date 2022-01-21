3d807f838f19a1f22f796ec7653dd2d5511e586d
Andrei Espelien <71854935+andreilespelien@users.noreply.github.com>
2022-01-21 11:24:37 -0800

    GameLocations and Player stub classes
    Player.py pretty much complete (storage class)

---
+++ GameLocations.py
@@ -0,0 +1,17 @@
+class GameLocations:
+    # cave = Cave()
+    playerPos = 0
+    wumpusPos = 0
+    hazards = []
+
+    def __init__(playerPos):
+        self.playerPos = playerPos
+    
+    def shootArrow():
+        pass
+
+    def warnPlayer():
+        pass
+
+    def getSecret():
+        pass
