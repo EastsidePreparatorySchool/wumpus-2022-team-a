bc3ea62680f974d3d1e47ba5217ca30171f011d2
hoshicoding <stippett@eastsideprep.org>
2022-01-21 11:49:38 -0800

    Create Trivia.py

---
+++ Trivia.py
@@ -0,0 +1,5 @@
+class Trivia:
+    def challenge(correct, attempts):
+        print("the player has " + str(attempts) + " tries to get " + str(correct) + " questions right")
+
+Trivia.challenge(3, 5)
