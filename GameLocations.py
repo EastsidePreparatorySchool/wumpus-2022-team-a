0ed9ab2dda226e80b6fc264172e8b3de720ff9fd
Andrei Espelien <71854935+andreilespelien@users.noreply.github.com>
2022-01-21 11:48:03 -0800

    shootArrow() updates
    Moves Wumpus using Lazy class

--- GameLocations.py
+++ GameLocations.py
@@ -1,14 +1,25 @@
+from numpy import true_divide
+
+
 class GameLocations:
     # cave = Cave()
     playerPos = 0
     wumpusPos = 0
+    wumpusState = "ASLEEP"
     hazards = []
 
     def __init__(playerPos):
         self.playerPos = playerPos
     
-    def shootArrow():
-        pass
+    def shootArrow(targetPos):
+        if targetPos == wumpusPos:
+            # GameControl.endGame()
+            return True
+        
+        wumpusState = "AWAKE"
+        for i in range(2):
+            LazyWumpusObject.moveWumpus()
+        return False
 
     def warnPlayer():
         pass
