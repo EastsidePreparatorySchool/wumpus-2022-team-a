a2673c02e2de82130a5af9288b3ff8a18fd52d93
AYNumber2 <ayuen@eastsideprep.org>
2022-01-28 09:49:16 -0800

    Update LazyWumpusObject.py

--- LazyWumpusObject.py
+++ LazyWumpusObject.py
@@ -1,5 +1,40 @@
 class LazyWumpus:
-    WUMPUS_STATE = "asleep"
+    wumpState = "ASLEEP"
+    wumpPos = 0
 
-    def checkState():
+    # accessor method for wumpPos for other objects
+    def getWumpPos():
+        return wumpPos
+    
+    # accessor method for wumpState for other objects
+    def getWumpState():
+        return wumpState
+
+    # call turn number from ?
+    def changeState():
+        global turnNum
+        if turnNum >
         pass
+
+    def moveWumpus():
+        if wumpPos % 2 == 1:
+            possiblePos = [
+                (wumpPos + 6) % 30, 
+                (wumpPos + 1),
+                (wumpPos - 5) % 30,
+                (wumpPos - 6) % 30,
+                (wumpPos - 7) % 30,
+                (wumpPos + 5) % 30
+            ]
+        else:
+            possiblePos = [
+                (wumpPos + 6) % 30, 
+                (wumpPos + 1),
+                (wumpPos - 5) % 30,
+                (wumpPos - 6) % 30,
+                (wumpPos - 7) % 30,
+                (wumpPos + 5) % 30
+            ]
+        pass
+
+    
