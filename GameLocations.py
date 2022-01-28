c0687a272ed32821a12295ee8042833498bc6e09
Andrei Espelien <71854935+andreilespelien@users.noreply.github.com>
2022-01-28 11:20:32 -0800

    Generate hazards and warnings
    Verify that this actually works

--- GameLocations.py
+++ GameLocations.py
@@ -1,8 +1,14 @@
 from numpy import true_divide
+import random
+
+# from GameControl import GameControl
+from cave import Cave
+from Player import Player
+from LazyWumpusObject import LazyWumpusObject
 
 
 class GameLocations:
-    # cave = Cave()
+    cave = Cave()
     playerPos = 0
     wumpusPos = 0
     wumpusState = "ASLEEP"
@@ -10,6 +16,13 @@ class GameLocations:
 
     def __init__(playerPos):
         self.playerPos = playerPos
+
+        hazardPos = random.sample(range(1, 31), 4)
+        hazards[hazardPos[0]] = "PIT"
+        hazards[hazardPos[1]] = "PIT"
+        hazards[hazardPos[2]] = "BAT"
+        hazards[hazardPos[3]] = "BAT"
+        
     
     def shootArrow(targetPos):
         if targetPos == wumpusPos:
@@ -22,7 +35,12 @@ class GameLocations:
         return False
 
     def warnPlayer():
-        pass
+        possibleCaves = Cave.get_connected()
+        possibleHazards = []
+
+        for cave in possibleCaves:
+            if cave in hazards.keys():
+                possibleHazard.append(hazards.get(cave))
 
     def getSecret():
         pass
