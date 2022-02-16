ea1ed99e94b3d843269a2299d28641673f124a0d
Parad0xial <64382080+Parad0xial@users.noreply.github.com>
2022-02-16 15:25:07 -0800

    Create GameController2.py

---
+++ GameController2.py
@@ -0,0 +1,45 @@
+import pygame
+
+from pygame.locals import *
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+
+background = pygame.image.load(r'C:\Users\sallen24\Downloads\koala.jpg')
+
+def drawBackground():
+    screen.blit(background, (0, 0))
+
+
+drawBackground()
+
+# Variable to keep our game loop runngameOn = True
+gameOn = True
+# Our game loop
+while gameOn:
+
+    # FPS (in ms delay)
+    pygame.time.delay(20)
+
+
+    # for loop through the event queue
+    for event in pygame.event.get():
+         
+        # Check for KEYDOWN event
+        if event.type == KEYDOWN:
+             
+            # If the Backspace key has been pressed set
+            # running to false to exit the main loop
+            if event.key == K_BACKSPACE:
+                gameOn = False
+                 
+        # Check for QUIT event
+        elif event.type == QUIT:
+            gameOn = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print("clickkk")
+
+
+
+    pygame.display.update()
